51bf0781b00bd2cc47a7c7bea9bac875f4addb56
Fedya001 <rabbitera@gmail.com>
2020-04-17 16:41:28 +0300

    Modularize MasterManager

--- mapreduce.cpp
+++ mapreduce.cpp
@@ -2,7 +2,6 @@
 
 #include <boost/filesystem.hpp>
 #include <boost/process.hpp>
-#include <fstream>
 #include <vector>
 #include <iostream>
 
@@ -17,13 +16,7 @@ MasterManager::MasterManager(std::string script_path, std::string src_file,
       dst_file_(std::move(dst_file)) {}
 
 void MasterManager::RunMappers(uint32_t count) const {
-  std::vector<std::string> records;
-
-  std::ifstream src(src_file_);
-  while (src.peek() != EOF) {
-    records.emplace_back();
-    std::getline(src, records.back());
-  }
+  auto records = ExtractRecords(src_file_);
 
   count = std::min(static_cast<size_t>(count), records.size());
   size_t records_per_map = records.size() / count;
@@ -32,19 +25,7 @@ void MasterManager::RunMappers(uint32_t count) const {
   std::vector<size_t> jobs_sizes(count, records_per_map);
   std::for_each(jobs_sizes.begin(), jobs_sizes.begin() + extra_records_number,
                 [](size_t& size) { ++size; });
-
-  std::vector<TmpFile> inputs;
-  inputs.reserve(count);
-  std::generate_n(std::back_inserter(inputs), count,
-                  [] { return TmpFile(std::ios::in | std::ios::out); });
-
-  size_t record_id = 0;
-  for (size_t map_id = 0; map_id < count; ++map_id) {
-    for (size_t i = 0; i < jobs_sizes[map_id]; ++i) {
-      inputs[map_id].Stream() << records[record_id++] << '\n';
-    }
-    inputs[map_id].Stream().flush();
-  }
+  auto inputs = SplitRecordsIntoFiles(records, jobs_sizes);
 
   std::vector<TmpFile> outputs;
   outputs.reserve(count);
@@ -64,14 +45,7 @@ void MasterManager::RunMappers(uint32_t count) const {
     child.wait();
   }
 
-  std::string buffer;
-  std::ofstream dst(dst_file_);
-  for (auto&& output : outputs) {
-    while (output.Stream().peek() != EOF) {
-      std::getline(output.Stream(), buffer);
-      dst << buffer << '\n';
-    }
-  }
+  JoinFiles(outputs, dst_file_);
 }
 
 void MasterManager::RunReducers() const {
@@ -81,6 +55,50 @@ void MasterManager::RunReducers() const {
 
 void MasterManager::Sort() const {
   // TODO : implement it
+}
+
+MasterManager::Records MasterManager::ExtractRecords(
+    const std::string& file) {
+  Records records;
+
+  std::ifstream src(file);
+  while (src.peek() != EOF) {
+    records.emplace_back();
+    std::getline(src, records.back());
+  }
+
+  return records;
+}
+
+std::vector<TmpFile> MasterManager::SplitRecordsIntoFiles(
+    const MasterManager::Records& records,
+    const std::vector<size_t>& jobs_sizes) {
+  std::vector<TmpFile> files;
+  files.reserve(jobs_sizes.size());
+  std::generate_n(std::back_inserter(files), jobs_sizes.size(),
+                  [] { return TmpFile(std::ios::in | std::ios::out); });
+
+  size_t record_id = 0;
+  for (size_t job_id = 0; job_id < jobs_sizes.size(); ++job_id) {
+    for (size_t i = 0; i < jobs_sizes[job_id]; ++i) {
+      files[job_id].Stream() << records[record_id++] << '\n';
+    }
+    files[job_id].Stream().flush();
+  }
+
+  return files;
+}
+
+void MasterManager::JoinFiles(std::vector<TmpFile>& files,
+                              const std::string& joined_file) {
+  std::string buffer;
+  std::ofstream joined(joined_file);
+  for (auto&& file : files) {
+    while (file.Stream().peek() != EOF) {
+      std::getline(file.Stream(), buffer);
+      joined << buffer << '\n';
+    }
+  }
 }
 
 } // namespace mapreduce
--- mapreduce.h
+++ mapreduce.h
@@ -7,6 +7,8 @@ namespace mapreduce {
 
 class MasterManager {
  public:
+  typedef std::vector<std::string> Records;
+
   MasterManager(std::string script_path, std::string src_file,
                 std::string dst_file);
 
@@ -15,6 +17,12 @@ class MasterManager {
 
  private:
   void Sort() const;
+
+  static Records ExtractRecords(const std::string& file);
+  static std::vector<TmpFile> SplitRecordsIntoFiles(
+      const Records& records, const std::vector<size_t>& jobs_sizes);
+  static void JoinFiles(std::vector<TmpFile>& files,
+                        const std::string& joined_file);
 
  private:
   std::string script_path_;
